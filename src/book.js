c7f4c8e66d0a35dd9bfff31dc8a404604b5615de
StevenTheBearded <>
2021-03-24 19:15:12 -0400

    book-test completed

--- src/book.js
+++ src/book.js
@@ -1,12 +1,53 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns){
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns};
+};
+
+function saveReview(review, reviews){
+  if(reviews.includes(review)){
+    return
+  }else{    
+    reviews.push(review)
+    }
+  }
+
+function calculatePageCount(bookTitle){
+  var pageAmount = bookTitle.length * 20
+  return pageAmount
 }
+
+function writeBook(bookTitle, bookCharacter, bookGenre){
+  var createBook = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: bookGenre,
+    pageCount: calculatePageCount(bookTitle)
+  }
+  return createBook
+}
+
+function editBook(title){
+  var pages = title.pageCount
+  var result = pages * .75
+  title.pageCount = result
+}
+
+
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
